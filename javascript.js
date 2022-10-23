63780636f19485ee082e1fa858595ddaf296d6f7
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2022-10-23 21:28:04 +0300

    css и штрихи

--- javascript.js
+++ javascript.js
@@ -22,55 +22,76 @@ document.getElementById('click_me').addEventListener('click', check)
 
 
 function checkRegistration() {
-    let result = document.getElementById('first_name').value;
-    if (result == '') {
-        alert('Вы не заполнили поле "Введите ваше имя"');
-        document.form.name.focus();
+    let first_name = document.getElementById('first_name').value;
+    let number = document.getElementById('number').value;
+    let tel = /^[A-Za-z]+$/;
+    let email = document.getElementById('email').value;
+    let email2 = /^[A-Za-z0-9]+$/;
+    let password = document.getElementById('password').value;
+    let min7 = document.getElementById('password').value;
+    let password2 = document.getElementById('password2').value;
+
+    errorMessage.innerHTML = "";
+
+    if (first_name == '') {
+        errorMessage.innerHTML =
+            `Вы не заполнили поле - Введите ваше имя<br>`;
+        return false;
+    }
+
+    if (number == '') {
+        errorMessage.innerHTML =
+            `Вы не заполнили поле - Введите ваш номер<br>`;
+        return false;
+    }
+
+    if (tel.test(document.getElementById("number").value)) {
+        errorMessage.innerHTML =
+            `Вы вставили буквы в поле - Введите ваш номер<br>`;
+        return false;
+    }
+
+    if (email == '') {
+        errorMessage.innerHTML =
+            `Вы не заполнили поле - Введите ваш email<br>`;
+        return false;
+    }
+
+    if (email2.test(document.getElementById("email").value)) {
+        errorMessage.innerHTML =
+            `Вы не вставили @ в поле - Введите ваш email<br>`;
+        return false;
+    }
+
+    if (password == '') {
+        errorMessage.innerHTML =
+            `Вы не заполнили поле - Введите ваш пароль<br>`;
+        return false;
+    }
+
+    if (min7.length <= 6) {
+        errorMessage.innerHTML =
+            `Слишком маленький пароль, введите минимум 7 символов<br>`;
         return false;
     }
 
 
-    let result2 = document.getElementById('number').value;
-    if (result2 == '') {
-        alert('Вы не заполнили поле "Введите ваш номер"');
-    }
-
-    let resalt3 = /^[A-Za-z]+$/;
-    if (resalt3.test(document.getElementById("number").value)) {
-        alert('Вы вставили буквы в поле "Введите ваш номер"');
-    }
-
-    let result4 = document.getElementById('email').value;
-    if (result4 == '') {
-        alert('Вы не заполнили поле "Введите ваш email"');
-    }
-
-    let resalt5 = /^[A-Za-z0-9]+$/;
-    if (resalt5.test(document.getElementById("email").value)) {
-        alert('Вы не вставили @ в поле "Введите ваш email"');
-    }
-
-    let result6 = document.getElementById('password').value;
-    if (result6 == '') {
-        alert('Вы не заполнили поле "Введите ваш пароль"');
-    }
-
-    let result7 = document.getElementById('password').value;
-    if (result7.length <= 6) {
-        alert('Слишком маленький пароль, введите минимум 7 символов');
+    if (password2 == '') {
+        errorMessage.innerHTML =
+            `Вы не заполнили поле - Повторите пароль<br>`;
+        return false;
     }
 
     if (document.getElementById('password').value == document.getElementById('password2').value) {
     }
     else {
         alert('Пароли не совпадают');
+        return false;
     }
 
-    let result8 = document.getElementById('password2').value;
-    if (result8 == '') {
-        alert('Вы не заполнили поле "Повторите пароль"');
-    }
 
+    if (document.getElementById('form').value == "") {
+    }
     else {
         getMessage()
     }
@@ -85,32 +106,34 @@ document.getElementById('click_registr').addEventListener('click', checkRegistra
 
 
 
-
-
-
-//document.querySelector(".form_number").addEventListener("keyup", function () {
-//    this.value = this.value.replace(/[^\d]/g, "");
-//});
-
-
 function selectChange() {
 
     let color = document.getElementById("color").value;
 
     if (color == "white") {
         document.body.style.backgroundColor = "White";
+        document.body.style.color = "Black";
     }
     else if (color == "dark") {
         document.body.style.backgroundColor = "Black";
+        document.body.style.color = "White";
     }
     else if (color == "blue") {
         document.body.style.backgroundColor = "Blue";
+        document.body.style.color = "White";
     }
     else if (color == "grey") {
         document.body.style.backgroundColor = "Grey";
+        document.body.style.color = "White";
     }
 }
 
 document.getElementById('select').addEventListener('click', selectChange)
 
+
+
+
+//document.querySelector(".form_number").addEventListener("keyup", function () {
+//    this.value = this.value.replace(/[^\d]/g, "");
+//});
 
